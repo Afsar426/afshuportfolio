fb9608f7dffb2e137755eefa9acf59bca207145c
Afsar azam <194261696+Afsar426@users.noreply.github.com>
2026-04-18 18:06:10 +0530

    Update vite.config.mjs

--- vite.config.mjs
+++ vite.config.mjs
@@ -1,8 +1,6 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// ✅ Correct Vite configuration for GitHub Pages deployment
 export default defineConfig({
   plugins: [react()],
-  base: process.env.NODE_ENV === 'production' ? '/afshuportfolio/' : '/', // 👈 correct repo name
 })
